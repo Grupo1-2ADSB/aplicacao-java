bcee29996fa998e062f70bcfbc88c3dffdb44bbb
AlexanderKimura <alexander.silva@sptech.school>
2023-04-16 17:48:30 -0300

    correcao script instalacao java e aplicacao na vm

--- smart-analytic-machine/bash.sh
+++ smart-analytic-machine/bash.sh
@@ -9,7 +9,7 @@ else
         echo "instalando java"
 
                 sudo apt install openjdk-17-jdk openjdk-17-jre -y
-        git clone https://github.com/Grupo1-2ADSB/aplicacao-java/
 fi
+        git clone https://github.com/Grupo1-2ADSB/aplicacao-java/
         cd aplicacao-java/smart-analytic-machine/target
         java -jar smart-analytic-machine-1.0-SNAPSHOT-jar-with-dependencies.jar
