d5538078b12de31dedf78048230aa033bfbe466e
leobr1t0 <leonardo.oliveira@sptech.school>
2023-04-21 22:42:22 -0300

    fix: Inserção de dados no banco

--- smart-analytic-machine/src/main/java/controller/LeituraController.java
+++ smart-analytic-machine/src/main/java/controller/LeituraController.java
@@ -19,35 +19,10 @@ import com.github.britooo.looca.api.group.servicos.Servico;
 import com.github.britooo.looca.api.group.servicos.ServicoGrupo;
 import com.github.britooo.looca.api.group.sistema.Sistema;
 import com.github.britooo.looca.api.group.temperatura.Temperatura;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
-;
-import model.LeituraModel;
-import org.springframework.jdbc.core.JdbcTemplate;
-import model.LeituraModel;
-import org.springframework.jdbc.core.JdbcTemplate;
-import model.LeituraModel;
-import org.springframework.jdbc.core.JdbcTemplate;
-import model.LeituraModel;
-import org.springframework.jdbc.core.JdbcTemplate;
-import model.LeituraModel;
-import org.springframework.jdbc.core.JdbcTemplate;
-import model.LeituraModel;
-import org.springframework.jdbc.core.JdbcTemplate;
-import model.LeituraModel;
-import org.springframework.jdbc.core.JdbcTemplate;
-import model.LeituraModel;
-import org.springframework.jdbc.core.JdbcTemplate;
-import model.LeituraModel;
-import org.springframework.jdbc.core.JdbcTemplate;
-import model.LeituraModel;
-import org.springframework.jdbc.core.JdbcTemplate;
-import model.LeituraModel;
-import org.springframework.jdbc.core.JdbcTemplate;
 import model.LeituraModel;
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -55,8 +30,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
  *
  * @author BELLA
  */
-
-
 public class LeituraController {
 
     public static void main(String[] args) {
@@ -88,90 +61,60 @@ public class LeituraController {
         leituraModel.setDataHoraLeitura(LocalDateTime.now());
 
         //inserindo a leitura da memoria em uso do looca
-        leituraModel.setLeitura(looca.getMemoria().getEmUso().doubleValue());
-
-        con.update("insert into tbLeitura values (?, ? ,?)",
-                null, leituraModel.getLeitura(), leituraModel.getDataHoraLeitura());
-
         System.out.println("----------Memoria----------");
-        System.out.println(leituraModel.getLeitura());
 
-        //inserindo a leitura processador
-        leituraModel.setLeitura(looca.getProcessador().getFrequencia().doubleValue());
-
+        //Uso memória
+        leituraModel.setLeitura(looca.getMemoria().getEmUso().doubleValue());
         con.update("insert into tbLeitura values (?, ? ,?)",
                 null, leituraModel.getLeitura(), leituraModel.getDataHoraLeitura());
 
-        System.out.println("----------Processador----------");
-        System.out.println(leituraModel.getLeitura());
+        //Memória disponível
+        leituraModel.setLeitura(looca.getMemoria().getDisponivel().doubleValue());
+        con.update("insert into tbLeitura values (?, ? ,?)",
+                null, leituraModel.getLeitura(), leituraModel.getDataHoraLeitura());
 
-        //inserindo leitura de rede
-        
+        //Memória total
+        leituraModel.setLeitura(looca.getMemoria().getTotal().doubleValue());
+        con.update("insert into tbLeitura values (?, ? ,?)",
+                null, leituraModel.getLeitura(), leituraModel.getDataHoraLeitura());
+
+        //---------------------------------------------------------------------------//
+        //inserindo a leitura processador
+        System.out.println("----------Processador----------");
+
+        //Frequência processador
+        leituraModel.setLeitura(looca.getProcessador().getFrequencia().doubleValue());
+        con.update("insert into tbLeitura values (?, ? ,?)",
+                null, leituraModel.getLeitura(), leituraModel.getDataHoraLeitura());
+
+        //Uso processador
+        leituraModel.setLeitura(looca.getProcessador().getUso().doubleValue());
+        con.update("insert into tbLeitura values (?, ? ,?)",
+                null, leituraModel.getLeitura(), leituraModel.getDataHoraLeitura());
+
+        //---------------------------------------------------------------------------//
+        //inserindo a leitura rede
         System.out.println("----------Rede----------");
+        
         System.out.println(looca.getRede().getGrupoDeInterfaces().getInterfaces());
         System.out.println(looca.getRede().getParametros().getHostName());
 
+        //---------------------------------------------------------------------------//
         //inserindo leitura de disco
         System.out.println("----------Disco----------");
-        System.out.println(looca.getGrupoDeDiscos().getDiscos());
-        System.out.println(looca.getGrupoDeDiscos().getQuantidadeDeDiscos());
-        //System.out.println("----------fim disco-------");
+        
+        //Tamanho total disco
+        leituraModel.setLeitura(looca.getGrupoDeDiscos().getTamanhoTotal().doubleValue());
+        con.update("insert into tbLeitura values (?, ? ,?)",
+                null, leituraModel.getLeitura(), leituraModel.getDataHoraLeitura());
 
-        Double[] teste = new Double[8];
-        List<Double> teste0 = new ArrayList<>();
-        List<Double> teste1 = new ArrayList<>();
-        List<Double> teste2 = new ArrayList<>();
-        List<Double> teste3 = new ArrayList<>();
-        List<Double> teste4 = new ArrayList<>();
-        List<Double> teste5 = new ArrayList<>();
-        List<Double> teste6 = new ArrayList<>();
-        List<Double> teste7 = new ArrayList<>();
+        //System.out.println(looca.getGrupoDeDiscos().getTamanhoTotal());
 
+        //---------------------------------------------------------------------------//
         new Timer().scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
-                Double teste55 = looca.getProcessador().getUso();
-                Integer teste25 = Integer.valueOf(teste55.toString().substring(0, 1));
 
-                System.out.println(teste25.TYPE);
-
-                switch (teste25) {
-                    case 0:
-                        teste0.add(Double.valueOf(teste25));
-                        break;
-                    case 1:
-                        teste1.add(Double.valueOf(teste25));
-                        break;
-                    case 2:
-                        teste2.add(Double.valueOf(teste25));
-                        break;
-                    case 3:
-                        teste3.add(Double.valueOf(teste25));
-                        break;
-                    case 4:
-                        teste4.add(Double.valueOf(teste25));
-                        break;
-                    case 5:
-                        teste5.add(Double.valueOf(teste25));
-                        break;
-                    case 6:
-                        teste6.add(Double.valueOf(teste25));
-                        break;
-                    case 7:
-                        teste7.add(Double.valueOf(teste25));
-                        break;
-                    default:
-                        throw new AssertionError();
-                }
-
-                System.out.println(teste0);
-                System.out.println(teste1);
-                System.out.println(teste2);
-                System.out.println(teste3);
-                System.out.println(teste4);
-                System.out.println(teste5);
-                System.out.println(teste6);
-                System.out.println(teste7);
             }
         }, 0, 1000);
     }
