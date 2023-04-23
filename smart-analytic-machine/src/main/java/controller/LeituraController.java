9a096b2f7013137f7e02450e0f4d84a1bc129087
leobr1t0 <leonardo.oliveira@sptech.school>
2023-04-23 14:59:55 -0300

    Teste ociosidade

--- smart-analytic-machine/src/main/java/controller/LeituraController.java
+++ smart-analytic-machine/src/main/java/controller/LeituraController.java
@@ -54,63 +54,63 @@ public class LeituraController {
         List<ServicoGrupo> redeInterface = new ArrayList();
         List<Servico> servico = new ArrayList();
 
-        // Instanciando Model de leitura - dados que vêm do looca
-        LeituraModel leituraModel = new LeituraModel();
-
-        //data e hora 
-        leituraModel.setDataHoraLeitura(LocalDateTime.now());
-
-        //inserindo a leitura da memoria em uso do looca
-        System.out.println("----------Memoria----------");
-
-        //Uso memória
-        leituraModel.setLeitura(looca.getMemoria().getEmUso().doubleValue());
-        con.update("insert into tbLeitura values (?, ? ,?)",
-                null, leituraModel.getLeitura(), leituraModel.getDataHoraLeitura());
-
-        //Memória disponível
-        leituraModel.setLeitura(looca.getMemoria().getDisponivel().doubleValue());
-        con.update("insert into tbLeitura values (?, ? ,?)",
-                null, leituraModel.getLeitura(), leituraModel.getDataHoraLeitura());
-
-        //Memória total
-        leituraModel.setLeitura(looca.getMemoria().getTotal().doubleValue());
-        con.update("insert into tbLeitura values (?, ? ,?)",
-                null, leituraModel.getLeitura(), leituraModel.getDataHoraLeitura());
-
-        //---------------------------------------------------------------------------//
-        //inserindo a leitura processador
-        System.out.println("----------Processador----------");
-
-        //Frequência processador
-        leituraModel.setLeitura(looca.getProcessador().getFrequencia().doubleValue());
-        con.update("insert into tbLeitura values (?, ? ,?)",
-                null, leituraModel.getLeitura(), leituraModel.getDataHoraLeitura());
-
-        //Uso processador
-        leituraModel.setLeitura(looca.getProcessador().getUso().doubleValue());
-        con.update("insert into tbLeitura values (?, ? ,?)",
-                null, leituraModel.getLeitura(), leituraModel.getDataHoraLeitura());
-
-        //---------------------------------------------------------------------------//
-        //inserindo a leitura rede
-        System.out.println("----------Rede----------");
-        
-        System.out.println(looca.getRede().getGrupoDeInterfaces().getInterfaces());
-        System.out.println(looca.getRede().getParametros().getHostName());
-
-        //---------------------------------------------------------------------------//
-        //inserindo leitura de disco
-        System.out.println("----------Disco----------");
-        
-        //Tamanho total disco
-        leituraModel.setLeitura(looca.getGrupoDeDiscos().getTamanhoTotal().doubleValue());
-        con.update("insert into tbLeitura values (?, ? ,?)",
-                null, leituraModel.getLeitura(), leituraModel.getDataHoraLeitura());
-
+//        // Instanciando Model de leitura - dados que vêm do looca
+//        LeituraModel leituraModel = new LeituraModel();
+//
+//        //data e hora 
+//        leituraModel.setDataHoraLeitura(LocalDateTime.now());
+//
+//        //inserindo a leitura da memoria em uso do looca
+//        System.out.println("----------Memoria----------");
+//
+//        //Uso memória
+//        leituraModel.setLeitura(looca.getMemoria().getEmUso().doubleValue());
+//        con.update("insert into tbLeitura values (?, ? ,?)",
+//                null, leituraModel.getLeitura(), leituraModel.getDataHoraLeitura());
+//
+//        //Memória disponível
+//        leituraModel.setLeitura(looca.getMemoria().getDisponivel().doubleValue());
+//        con.update("insert into tbLeitura values (?, ? ,?)",
+//                null, leituraModel.getLeitura(), leituraModel.getDataHoraLeitura());
+//
+//        //Memória total
+//        leituraModel.setLeitura(looca.getMemoria().getTotal().doubleValue());
+//        con.update("insert into tbLeitura values (?, ? ,?)",
+//                null, leituraModel.getLeitura(), leituraModel.getDataHoraLeitura());
+//
+//        //---------------------------------------------------------------------------//
+//        //inserindo a leitura processador
+//        System.out.println("----------Processador----------");
+//
+//        //Frequência processador
+//        leituraModel.setLeitura(looca.getProcessador().getFrequencia().doubleValue());
+//        con.update("insert into tbLeitura values (?, ? ,?)",
+//                null, leituraModel.getLeitura(), leituraModel.getDataHoraLeitura());
+//
+//        //Uso processador
+//        leituraModel.setLeitura(looca.getProcessador().getUso().doubleValue());
+//        con.update("insert into tbLeitura values (?, ? ,?)",
+//                null, leituraModel.getLeitura(), leituraModel.getDataHoraLeitura());
+//
+//        //---------------------------------------------------------------------------//
+//        //inserindo a leitura rede
+//        System.out.println("----------Rede----------");
+//
+//        System.out.println(looca.getRede().getGrupoDeInterfaces().getInterfaces());
+//        System.out.println(looca.getRede().getParametros().getHostName());
+//
+//        //---------------------------------------------------------------------------//
+//        //inserindo leitura de disco
+//        System.out.println("----------Disco----------");
+//
+//        //Tamanho total disco
+//        leituraModel.setLeitura(looca.getGrupoDeDiscos().getTamanhoTotal().doubleValue());
+//        con.update("insert into tbLeitura values (?, ? ,?)",
+//                null, leituraModel.getLeitura(), leituraModel.getDataHoraLeitura());
         //System.out.println(looca.getGrupoDeDiscos().getTamanhoTotal());
-
         //---------------------------------------------------------------------------//
+        //Tempo Ocioso
+
         new Timer().scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
